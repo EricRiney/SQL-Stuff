11744b3798735efda98961d98dcf9fc4b6a289be
EricRiney <fails.to.install@gmail.com>
2016-05-20 12:22:31 -0700

    mirroring

---
+++ mirroring.sql
@@ -0,0 +1,5 @@
+-- This feature will be removed in a future version of Microsoft SQL Server. 
+-- Avoid using this feature in new development work, 
+-- and plan to modify applications that currently use this feature. 
+-- Use Always On Availability Groups instead.
+
